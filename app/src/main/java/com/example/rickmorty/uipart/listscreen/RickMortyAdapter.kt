56beef0e08e47ee4c71d55acc0decab52fed731c
mcicishvilii <83747561+mcicishvilii@users.noreply.github.com>
2022-05-13 18:58:19 +0400

    //

--- app/src/main/java/com/example/rickmorty/uipart/listscreen/RickMortyAdapter.kt
+++ app/src/main/java/com/example/rickmorty/uipart/listscreen/RickMortyAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.rickmorty.uipart.listscreen
 
+import android.R.attr.fadeDuration
+import android.R.attr.scaleType
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rickmorty.R
 import com.example.rickmorty.RickMortyModels.json.Result
 import com.example.rickmorty.databinding.RickMortyItemLayoutBinding
+import com.facebook.drawee.generic.GenericDraweeHierarchy
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder
 
 
 class RickMortyAdapter(val rickMortyList: MutableList<Result>):RecyclerView.Adapter<RickMortyViewHolder>() {
@@ -30,9 +34,10 @@ class RickMortyAdapter(val rickMortyList: MutableList<Result>):RecyclerView.Adap
         holder.binding.avatar.setImageURI("https://rickandmortyapi.com/api/character/avatar/${item.id}.jpeg")
         holder.binding.characterName.text = item.name
         holder.binding.GenderAndRace.text = "${item.status} - ${item.species}"
-        holder.binding.lastKnownLocation.text = "Last Known Location \n${item.location.name}"
+        holder.binding.lastKnownLocation.text = "${item.location.name}"
 
         if (item.gender == "Male"){
+
             holder.binding.genderSing.setImageURI("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRASGnAmFZp3Ls9bK4kYmICpNd0b9OnJTrzIg&usqp=CAU")
         }else if (item.gender == "Female"){
             holder.binding.genderSing.setImageURI("https://cdn1.iconfinder.com/data/icons/medicine-pt-6/100/087_-_woman_gender_sex_female_gender_symbol-512.png")
@@ -52,8 +57,9 @@ class RickMortyAdapter(val rickMortyList: MutableList<Result>):RecyclerView.Adap
     override fun getItemCount(): Int {
         return rickMortyList.size
     }
-
 }
+
+
 
 class RickMortyViewHolder(val binding: RickMortyItemLayoutBinding) :
     RecyclerView.ViewHolder(binding.root) {
